dd8e9a47f912f4a594a672e4a128aa4bef9436ed
ganeshbs98 <siddu0335@gmail.com>
2024-08-04 20:57:08 +0530

    tuples python

---
+++ tuples/bassic.py
@@ -0,0 +1,11 @@
+# tuple=immutable
+
+my_tuple=(1,3,4,5,6,7,"syed","s"," ")#Hashable
+print(my_tuple)
+print(type(my_tuple))
+
+# iteration
+
+for i in my_tuple:
+    print(i)
+
---
+++ tuples/methods.py
@@ -0,0 +1,8 @@
+my_tuple=(1,3,5,7,5,6,"ganesh",True,"jnsjw",5,6,7,5,4,5)
+
+# there are 2 methods
+# count()-return the number of tyms the given value occured
+# index()- returns the index of the value frist occurence
+
+print(my_tuple.count(5))
+print(my_tuple.index(5))
